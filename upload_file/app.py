ea05819ead301e92d03863a4810e9585d2a2cbe7
Yashy2k <yash.panchal@colorado.edu>
2023-11-24 06:50:12 -0700

    Added a function to save images with unique names

--- upload_file/app.py
+++ upload_file/app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, request,send_from_directory
+import time
+from flask import Flask, render_template, request, send_from_directory
 from PIL import Image
 import numpy as np
 import torch
@@ -44,21 +45,66 @@ def load_model(model_path):
     return model
 
 # Load the model once
-model = load_model(r'H:\DCSC\ObjectSense\model\retina_net.pkl')
+model_path = r'H:\DCSC\ObjectSense\model\retina_net.pkl'
+model = load_model(model_path)
 
 # Detection threshold
 detection_threshold = 0.5
+
+def generate_unique_filename():
+    # Use timestamp for unique filenames
+    timestamp = time.time()
+    uploaded_image = f'{timestamp:.0f}_uploaded.jpg'
+    result_image = f'{timestamp:.0f}_result_image.jpg'
+    return uploaded_image, result_image
+
+def process_prediction(image, model, detection_threshold):
+    with torch.no_grad():
+        outputs = model(image)
+
+    scores = list(outputs[0]['scores'].detach().cpu().numpy())
+    boxes = outputs[0]['boxes'].detach().cpu().numpy()
+    labels = outputs[0]['labels'].cpu().numpy()
+
+    thresholded_preds_indices = [i for i, score in enumerate(scores) if score > detection_threshold]
+    filtered_boxes = boxes[thresholded_preds_indices]
+    filtered_classes = labels[thresholded_preds_indices]
+
+    return filtered_boxes, filtered_classes
+
+def draw_boxes(image, boxes, classes, coco_names):
+    image_np = np.array(image)
+
+    for i, box in enumerate(boxes):
+        if 0 <= classes[i] < len(coco_names):
+            color = COLORS[classes[i]]
+            cv2.rectangle(
+                image_np,
+                (int(box[0]), int(box[1])),
+                (int(box[2]), int(box[3])),
+                color, 2
+            )
+            cv2.putText(image_np, coco_names[classes[i]], (int(box[0]), int(box[1] - 5)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2,
+                        lineType=cv2.LINE_AA)
+        else:
+            print(f"Invalid class index: {classes[i]}")
+
+    return Image.fromarray(image_np)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST' and 'image' in request.files:
         image = request.files['image']
 
+        # Generate unique filenames using timestamp
+        uploaded_image_filename, result_image_filename = generate_unique_filename()
+
         # Save the uploaded image
-        uploaded_image_path = os.path.join(app.config['UPLOAD_FOLDER'], 'uploaded_image.jpg')
+        uploaded_image_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_image_filename)
         image.save(uploaded_image_path)
 
-        # Preprocess the image for the model
+        # Process the image
         img = Image.open(image).convert('RGB')
         img_tensor = F.to_tensor(img).unsqueeze(0)
 
@@ -68,55 +114,15 @@ def index():
 
         # Process the prediction to draw bounding boxes on the image
         result_image = draw_boxes(img, boxes, pred_classes, coco_names)
-
+        
         # Save the result image
-        result_image_path = os.path.join(app.config['OUTPUT_FOLDER'], 'result_image.jpg')
+        result_image_path = os.path.join(app.config['OUTPUT_FOLDER'], result_image_filename)
         result_image.save(result_image_path)
 
-        return render_template('index.html', uploaded_image=uploaded_image_path, result_image=result_image_path)
+        return render_template('index.html', uploaded_image=uploaded_image_filename, result_image=result_image_filename)
 
     return render_template('index.html', uploaded_image=None, result_image=None)
 
-def process_prediction(image, model, detection_threshold):
-    # Run the model on the image
-    with torch.no_grad():
-        outputs = model(image)
-
-    # Get the predicted scores, boxes, and labels
-    scores = list(outputs[0]['scores'].detach().cpu().numpy())
-    boxes = outputs[0]['boxes'].detach().cpu().numpy()
-    labels = outputs[0]['labels'].cpu().numpy()
-
-    # Apply threshold to filter predictions
-    thresholded_preds_indices = [i for i, score in enumerate(scores) if score > detection_threshold]
-
-    # Filter predictions based on threshold
-    filtered_boxes = boxes[thresholded_preds_indices]
-    filtered_classes = labels[thresholded_preds_indices]
-
-    return filtered_boxes, filtered_classes
-
-def draw_boxes(image, boxes, classes, coco_names):
-    image_np = np.array(image)  # Convert PIL Image to numpy array
-    
-    for i, box in enumerate(boxes):
-        # Check if the class index is within the valid range
-        if 0 <= classes[i] < len(coco_names):
-            color = COLORS[classes[i]]
-            cv2.rectangle(
-                image_np,
-                (int(box[0]), int(box[1])),
-                (int(box[2]), int(box[3])),
-                color, 2
-            )
-            cv2.putText(image_np, coco_names[classes[i]], (int(box[0]), int(box[1]-5)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2,
-                        lineType=cv2.LINE_AA)
-        else:
-            print(f"Invalid class index: {classes[i]}")
-
-    return Image.fromarray(image_np)  # Convert back to PIL Image
-    
 @app.route('/uploads/<filename>')
 def uploaded_image(filename):
     return send_from_directory(app.root_path, filename)
